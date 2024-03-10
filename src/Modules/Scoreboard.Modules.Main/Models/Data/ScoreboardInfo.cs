81c32e0f1f373c52eacda5f40a4df79d246721ef
Edalik <www.ayakubov@yandex.ru>
2024-03-10 18:57:33 +0300

    xD

--- src/Modules/Scoreboard.Modules.Main/Models/Data/ScoreboardInfo.cs
+++ src/Modules/Scoreboard.Modules.Main/Models/Data/ScoreboardInfo.cs
@@ -1,57 +1,56 @@
 ﻿using System.IO;
 
-namespace Scoreboard.Modules.Main.Models.Data
+namespace Scoreboard.Modules.Main.Models.Data;
+
+public class ScoreboardInfo
 {
-    public class ScoreboardInfo
+    string[] Name =
+        {"HOME_TEAM",
+        "GUEST_TEAM",
+        "PERIOD",
+        "TIME_MINUTES",
+        "TIME_SECONDS",
+        "HOME_SCORE",
+        "GUEST_SCORE",
+        "HOME_PENALTY1_NUM",
+        "HOME_PENALTY1_TIME_MINUTES",
+        "HOME_PENALTY1_TIME_SECONDS",
+        "HOME_PENALTY2_NUM",
+        "HOME_PENALTY2_TIME_MINUTES",
+        "HOME_PENALTY2_TIME_SECONDS",
+        "GUEST_PENALTY1_NUM",
+        "GUEST_PENALTY1_TIME_MINUTES",
+        "GUEST_PENALTY1_TIME_SECONDS",
+        "GUEST_PENALTY2_NUM",
+        "GUEST_PENALTY2_TIME_MINUTES",
+        "GUEST_PENALTY2_TIME_SECONDS",};
+
+    string[] Value = new string[19];
+
+    public string GetName(int id)
     {
-        string[] Name =
-            {"HOME_TEAM",
-            "GUEST_TEAM",
-            "PERIOD",
-            "TIME_MINUTES",
-            "TIME_SECONDS",
-            "HOME_SCORE",
-            "GUEST_SCORE",
-            "HOME_PENALTY1_NUM",
-            "HOME_PENALTY1_TIME_MINUTES",
-            "HOME_PENALTY1_TIME_SECONDS",
-            "HOME_PENALTY2_NUM",
-            "HOME_PENALTY2_TIME_MINUTES",
-            "HOME_PENALTY2_TIME_SECONDS",
-            "GUEST_PENALTY1_NUM",
-            "GUEST_PENALTY1_TIME_MINUTES",
-            "GUEST_PENALTY1_TIME_SECONDS",
-            "GUEST_PENALTY2_NUM",
-            "GUEST_PENALTY2_TIME_MINUTES",
-            "GUEST_PENALTY2_TIME_SECONDS",};
+        return Name[id];
+    }
 
-        string[] Value = new string[19];
+    public string GetValue(int id)
+    {
+        return Value[id];
+    }
 
-        public string GetName(int id)
+    public void SetValue(int id, string value)
+    {
+        Value[id] = value;
+    }
+
+    public void SaveValue(int id, string path)
+    {
+        if (Value[id] == null)
+            return;
+
+        using (StreamWriter logWriter = new StreamWriter($"{path}\\{Name[id]}.txt"))
         {
-            return Name[id];
-        }
-
-        public string GetValue(int id)
-        {
-            return Value[id];
-        }
-
-        public void SetValue(int id, string value)
-        {
-            Value[id] = value;
-        }
-
-        public void SaveValue(int id, string path)
-        {
-            if (Value[id] == null)
-                return;
-
-            using (StreamWriter logWriter = new StreamWriter($"{path}\\{Name[id]}.txt"))
-            {
-                logWriter.Write(Value[id]);
-                logWriter.Close();
-            }
+            logWriter.Write(Value[id]);
+            logWriter.Close();
         }
     }
 }
